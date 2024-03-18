43d81380b402bc356733bd7b028a539219911c8c
lattesprezzo <lattesprezzo@gmail.com>
2024-03-18 15:49:44 +0200

    Input building

--- Assets/Scripts/PlayerInputControl.cs
+++ Assets/Scripts/PlayerInputControl.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,19 +6,27 @@ public class PlayerInputControl : MonoBehaviour
 
     [Header("Input Action Asset")]
     [SerializeField] private InputActionAsset playerControls;
-    [Header("Input Action Asset")]
+    [Header("Input Map")]
     [SerializeField] private string actionMapName = "Player";
-    [Header("Input Action Asset")]
+    [Header("Actions")]
     [SerializeField] private string move = "Move";
-         [SerializeField] private string run = "Run";
+    [SerializeField] private string run = "Run";
 
+    private InputAction moveAction;
+    private InputAction runAction;  
+
+    public Vector2 MoveInput { get; private set; }
+    public float RunValue { get; private set; }
+    public bool RunInput { get; private set; }
+
+    public static PlayerInput Instance { get; private set; }    
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+
     }
 }
